d1e769f6cd836343c9ea5d09d484ea992fd0fcbd
RudraGoti <rudragoti.work@gmail.com>
2025-12-08 13:23:51 +0530

    Create Relative Rank.java

---
+++ Sorting/Relative Rank.java
@@ -0,0 +1,30 @@
+// Problem: Relative Rank.java
+// Platform: LeetCode
+// Time Complexity: O(n log n)
+// Space Complexity: O(n)
+
+class Solution {
+    public String[] findRelativeRanks(int[] score) {
+        int n = score.length;
+        String[] ans = new String[n];
+
+        int[][] arr = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            arr[i][0] = score[i]; 
+            arr[i][1] = i;        
+        }
+
+        Arrays.sort(arr, (a, b) -> b[0] - a[0]); 
+
+        for (int i = 0; i < n; i++) {
+            int idx = arr[i][1];
+
+            if (i == 0) ans[idx] = "Gold Medal";
+            else if (i == 1) ans[idx] = "Silver Medal";
+            else if (i == 2) ans[idx] = "Bronze Medal";
+            else ans[idx] = String.valueOf(i + 1);
+        }
+
+        return ans;
+    }
+}
