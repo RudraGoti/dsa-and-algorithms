4a89d1a7df25f5fe5e31984aef3d4cca877b5ebb
Rudra Mukeshbhai Goti <rudragoti.work@gmail.com>
2025-12-20 09:25:34 +0530

    Create Search insert position of K in a sorted array.cpp

---
+++ Searching/Search insert position of K in a sorted array.cpp
@@ -0,0 +1,27 @@
+// Problem: Search insert position of K in a sorted array
+// Platform: GeeksforGeeks
+// Time Complexity: O(log n)
+// Space Complexity: O(1)
+
+class Solution {
+  public:
+    int searchInsertK(vector<int> &arr, int k) {
+        int low = 0, high = arr.size() - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == k) {
+                return mid;
+            }
+            else if (arr[mid] < k) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+        
+        return low;
+    }
+};
