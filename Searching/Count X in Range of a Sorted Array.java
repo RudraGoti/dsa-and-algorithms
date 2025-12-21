dcf93b8dfb6f99d3751f0a416c9afb2bf89bf008
Rudra Mukeshbhai Goti <rudragoti.work@gmail.com>
2025-12-21 10:44:58 +0530

    Create Count X in Range of a Sorted Array.java

---
+++ Searching/Count X in Range of a Sorted Array.java
@@ -0,0 +1,61 @@
+// Problem: Count X in Range of a Sorted Array
+// Platform: GeeksforGeeks
+// Time Complexity: O(n + q log n)
+// Space Complexity: O(n)
+
+  class Solution {
+      public ArrayList<Integer> countXInRange(int[] arr, int[][] queries) {
+      
+          HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
+  
+          for (int i = 0; i < arr.length; i++) {
+              map.computeIfAbsent(arr[i], k -> new ArrayList<>()).add(i);
+          }
+  
+          ArrayList<Integer> ans = new ArrayList<>();
+  
+          for (int[] q : queries) {
+              int l = q[0];
+              int r = q[1];
+              int x = q[2];
+  
+              if (!map.containsKey(x)) {
+                  ans.add(0);
+                  continue;
+              }
+  
+              ArrayList<Integer> list = map.get(x);
+  
+              int left = lowerBound(list, l);
+              int right = upperBound(list, r);
+  
+              ans.add(right - left);
+          }
+  
+          return ans;
+      }
+  
+      private int lowerBound(ArrayList<Integer> list, int target) {
+          int low = 0, high = list.size();
+          while (low < high) {
+              int mid = low + (high - low) / 2;
+              if (list.get(mid) < target)
+                  low = mid + 1;
+              else
+                  high = mid;
+          }
+          return low;
+      }
+  
+      private int upperBound(ArrayList<Integer> list, int target) {
+          int low = 0, high = list.size();
+          while (low < high) {
+              int mid = low + (high - low) / 2;
+              if (list.get(mid) <= target)
+                  low = mid + 1;
+              else
+                  high = mid;
+          }
+          return low;
+      }
+  }
