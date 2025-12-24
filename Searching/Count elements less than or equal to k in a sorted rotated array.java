772578e62d8bdebf23cfd142427d483b630fb70c
Rudra Mukeshbhai Goti <rudragoti.work@gmail.com>
2025-12-24 10:33:35 +0530

    Create Count elements less than or equal to k in a sorted rotated array.java

---
+++ Searching/Count elements less than or equal to k in a sorted rotated array.java
@@ -0,0 +1,18 @@
+// Problem: Count elements less than or equal to k in a sorted rotated array
+// Platform: GeeksforGeeks
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+    public int countLessEqual(int[] arr, int x) {
+        
+        int c = 0;
+        
+        for(int i = 0; i < arr.length ; i++){
+            if(arr[i] <= x){
+                c++;
+            }
+        }
+        return c;
+    }
+}
