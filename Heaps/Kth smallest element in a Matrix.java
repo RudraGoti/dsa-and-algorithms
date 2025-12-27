747932c977d2bae65f0bb47725794ce31276c5ec
Rudra Mukeshbhai Goti <rudragoti.work@gmail.com>
2025-12-27 09:09:25 +0530

    Create Kth smallest element in a Matrix.java

---
+++ Heaps/Kth smallest element in a Matrix.java
@@ -0,0 +1,40 @@
+// Problem: Kth smallest element in a Matrix
+// Platform: GeeksforGeeks
+// Time Complexity: O(n log(max-min))
+// Space Complexity: O(1) 
+
+class Solution {
+    public int kthSmallest(int[][] mat, int k) {
+        int n = mat.length;
+        int low = mat[0][0];
+        int high = mat[n-1][n-1];
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            int count = countLessEqual(mat, mid);
+
+            if (count < k) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low;
+    }
+
+    private int countLessEqual(int[][] mat, int mid) {
+        int n = mat.length;
+        int i = n - 1, j = 0;
+        int count = 0;
+
+        while (i >= 0 && j < n) {
+            if (mat[i][j] <= mid) {
+                count += i + 1;
+                j++;
+            } else {
+                i--;
+            }
+        }
+        return count;
+    }
+}
