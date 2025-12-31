429bcb6f3af12f9c67141989d6870d644e24d6c1
Rudra Mukeshbhai Goti <rudragoti.work@gmail.com>
2025-12-31 10:28:01 +0530

    Create Longest Common Prefix.java

---
+++ Arrays/Longest Common Prefix.java
@@ -0,0 +1,21 @@
+Problem: Longest Common Prefix
+Platform: Leetcode
+Time Complexity: O(n × m)
+Space Complexity: O(1)
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+
+        String prefix = strs[0];
+
+        for (int i = 1; i < strs.length; i++) {
+            while (!strs[i].startsWith(prefix)) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        }
+
+        return prefix;
+    }
+}
