d636e9bff8cc8969cc99ca7d028821f5cb690c09
Rudra Mukeshbhai Goti <rudragoti.work@gmail.com>
2026-01-01 11:21:38 +0530

    Create Plus One.py

---
+++ Arrays/Plus One.py
@@ -0,0 +1,13 @@
+Problem: Plus One
+Platform: Leetcode
+Time Complexity: O(n)
+Space Complexity: O(1)
+
+class Solution(object):
+    def plusOne(self, digits):
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+        return [1] + digits
