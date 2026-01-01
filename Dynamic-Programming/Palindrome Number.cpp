21ff55ca37af2652284ff59b995517d6d4ea9d47
Rudra Mukeshbhai Goti <rudragoti.work@gmail.com>
2026-01-01 13:40:15 +0530

    Create Palindrome Number.cpp

---
+++ Dynamic-Programming/Palindrome Number.cpp
@@ -0,0 +1,19 @@
+Problem: Palindrome Number
+Platform: Leetcode 
+Time Complexity: O(log10 x) 
+Space Complexity: O(1) 
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+
+        int rev = 0;
+        while (x > rev) {
+            rev = rev * 10 + x % 10;
+            x /= 10;
+        }
+
+        return x == rev || x == rev / 10;
+    }
+};
