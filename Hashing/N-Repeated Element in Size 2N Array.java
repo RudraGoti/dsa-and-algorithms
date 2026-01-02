743beea332b4289b777b9be45ea270322db97bb8
Rudra Mukeshbhai Goti <rudragoti.work@gmail.com>
2026-01-02 11:15:24 +0530

    Create N-Repeated Element in Size 2N Array.java

---
+++ Hashing/N-Repeated Element in Size 2N Array.java
@@ -0,0 +1,16 @@
+Problem: N-Repeated Element in Size 2N Array.java
+Platform: LeetCode
+Time Complexity: O(n)
+Space Complexity: O(1)
+
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        int len = nums.length;
+        for (int i = 0; i < len - 1; i++) {
+            if (nums[i] == nums[i + 1]) return nums[i];
+            if (i + 2 < len && nums[i] == nums[i + 2]) return nums[i];
+            if (i + 3 < len && nums[i] == nums[i + 3]) return nums[i];
+        }
+        return 1;
+    }
+}
