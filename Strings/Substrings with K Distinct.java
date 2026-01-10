962a16057c22bc97655a9722b4609c81f4c2b56a
Rudra Mukeshbhai Goti <rudragoti.work@gmail.com>
2026-01-10 22:31:28 +0530

    Create Substrings with K Distinct.java

---
+++ Strings/Substrings with K Distinct.java
@@ -0,0 +1,33 @@
+Problem: Substrings with K Distinct
+Platform: GeeksforGeeks
+Time Complexity: O(n) 
+Space Complexity: O(1) 
+
+
+class Solution {
+    public int countSubstr(String s, int k) {
+        if (k == 0) return 0;
+        return atMostK(s, k) - atMostK(s, k - 1);
+    }
+
+    private int atMostK(String s, int k) {
+        int[] freq = new int[26];
+        int left = 0, distinct = 0, res = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            if (freq[s.charAt(right) - 'a'] == 0)
+                distinct++;
+            freq[s.charAt(right) - 'a']++;
+
+            while (distinct > k) {
+                freq[s.charAt(left) - 'a']--;
+                if (freq[s.charAt(left) - 'a'] == 0)
+                    distinct--;
+                left++;
+            }
+
+            res += (right - left + 1);
+        }
+        return res;
+    }
+}
