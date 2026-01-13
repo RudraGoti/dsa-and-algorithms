ad622456bac71fc73628afbbbd738d62eb39d08d
Rudra Mukeshbhai Goti <rudragoti.work@gmail.com>
2026-01-13 11:51:30 +0530

    Create Minimum Time Visiting All Points.java

---
+++ Arrays/Minimum Time Visiting All Points.java
@@ -0,0 +1,18 @@
+Problem: Minimum Time Visiting All Points
+Platform: LeetCode
+Time Complexity: O(n)
+Space Complexity: O(1)
+
+class Solution {
+    public int minTimeToVisitAllPoints(int[][] points) {
+        int time = 0;
+
+        for (int i = 1; i < points.length; i++) {
+            int dx = Math.abs(points[i][0] - points[i - 1][0]);
+            int dy = Math.abs(points[i][1] - points[i - 1][1]);
+            time += Math.max(dx, dy);
+        }
+
+        return time;
+    }
+}
