576cfb2ef57cf0a6cfef993218b2322e44df5636
Rudra Mukeshbhai Goti <rudragoti.work@gmail.com>
2026-01-29 17:37:30 +0530

    Create Stream First Non-repeating.cpp

---
+++ Stack-Queue/Stream First Non-repeating.cpp
@@ -0,0 +1,27 @@
+Problem: Stream First Non-repeating
+Platform: GeeksforGeeks 
+Time Complexity: O(n) 
+Space Complexity: O(1) 
+
+class Solution {
+  public:
+    string firstNonRepeating(string &s) {
+        vector<int> freq(26, 0);
+        queue<char> q;
+        string ans = "";
+        
+        for (char c : s) {
+            freq[c - 'a']++;
+            q.push(c);
+            
+            while (!q.empty() && freq[q.front() - 'a'] > 1) {
+                q.pop();
+            }
+            
+            if (q.empty()) ans.push_back('#');
+            else ans.push_back(q.front());
+        }
+        
+        return ans;
+    }
+};
