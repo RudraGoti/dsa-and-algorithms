ad857d0c45c678ebe2259175151daa7cf01a3b65
Rudra Mukeshbhai Goti <rudragoti.work@gmail.com>
2026-02-06 10:00:57 +0530

    Create Minimum Removals to Balance Array.cpp

---
+++ Sorting/Minimum Removals to Balance Array.cpp
@@ -0,0 +1,23 @@
+Problem: Minimum Removals to Balance Array
+Platform: Leetcode
+Time Complexity: O(n log n)
+Space Complexity: O(1) 
+
+
+class Solution {
+public:
+    int minRemoval(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int maxLen = 1;
+        int j = 0;
+
+        for (int i = 0; i < n; i++) {
+            while (j < n && (long long)nums[j] <= (long long)nums[i] * k) {
+                j++;
+            }
+            maxLen = max(maxLen, j - i);
+        }
+        return n - maxLen;
+    }
+};
