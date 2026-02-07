12dfc6c4e421585d9b939a2e8886f99112294a98
Rudra Mukeshbhai Goti <rudragoti.work@gmail.com>
2026-02-07 10:21:08 +0530

    Create Max sum in the configuration.java

---
+++ Arrays/Max sum in the configuration.java
@@ -0,0 +1,27 @@
+Problem: Max sum in the configuration
+Platform: GeeksforGeeks
+Time Complexity: O(n) 
+Space Complexity: O(1) 
+
+
+class Solution {
+    int maxSum(int[] arr) {
+        int n = arr.length;
+        int arrSum = 0;
+        int currVal = 0;
+
+        for (int i = 0; i < n; i++) {
+            arrSum += arr[i];
+            currVal += i * arr[i];
+        }
+
+        int maxVal = currVal;
+
+        for (int i = 1; i < n; i++) {
+            currVal = currVal + arrSum - n * arr[n - i];
+            maxVal = Math.max(maxVal, currVal);
+        }
+
+        return maxVal;
+    }
+}
