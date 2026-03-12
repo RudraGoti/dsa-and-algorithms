e5f33bb4addb78743cbdeec24c02bb4ab234da83
Rudra Mukeshbhai Goti <rudragoti.work@gmail.com>
2026-03-12 10:00:36 +0530

    Create Find Kth Bit in Nth Binary String.cpp

---
+++ Simulation/Find Kth Bit in Nth Binary String.cpp
@@ -0,0 +1,22 @@
+Problem: Find Kth Bit in Nth Binary String
+Platform: LeetCode
+Time Complexity: O(n)
+Space Complexity: O(n)
+
+
+class Solution {
+public:
+    char findKthBit(int n, int k) {
+        if(n == 1) return '0';
+
+        int mid = 1 << (n - 1);
+
+        if(k == mid) return '1';
+
+        if(k < mid) 
+            return findKthBit(n - 1, k);
+
+        char c = findKthBit(n - 1, (1 << n) - k);
+        return c == '0' ? '1' : '0';
+    }
+};
